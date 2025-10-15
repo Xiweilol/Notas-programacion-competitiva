1f0be26231372cc2ea5e84a76cbe537475711d96
Xiweilol <huangg.xiwei@gmail.com>
2025-10-15 16:30:33 -0600

    cambios 15-10-25. 4:30

---
+++ curso_c++/codeforces/B 2060.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int t; cin >> t;
+
+    while(t--){
+        int n,m; cin >> n >> m;
+        map <int,int> mapa;
+        bool pas = true;
+        for(int i = 0; i < n; i++){
+            vector <int> arr(m);
+            for(int j = 0; j < m; j++){
+                cin >> arr[j];
+            }
+            sort(arr.begin(),arr.end());
+
+        
+            for(int x = 1; x < (int)arr.size(); x++){
+                if(arr[x] != arr[x-1] + n){
+                    pas = false;
+                    break;
+                }
+            }
+
+            if(!pas){
+                break;
+            }
+
+            mapa[arr[0]] = i + 1;
+
+        }
+        if(pas){
+            for(auto it = mapa.begin(); it != mapa.end();it++){
+                cout << it->second << " ";
+            }
+            cout << "\n";
+        } else {
+            cout << -1 << "\n";
+        }
+        
+
+    }
+    return 0;
+}
