c1e6d8522780996f833f9d00d4c0c6b4e91ae6be
Xiwei <huangg.xiwei@gmail.com>
2025-10-22 11:47:40 -0600

    Inicie forest queries

---
+++ curso_c++/Club algoritmia/1652 CSES.cpp
@@ -0,0 +1,20 @@
+// Autor: Mintwi
+// Fecha: 2025-10-22
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n,q; cin >> n >> q;
+
+    vector <string> g(n+1,".");
+
+    for(int i = 1; i <= n; i++ ){
+        
+    }
+
+    return 0;
+}
