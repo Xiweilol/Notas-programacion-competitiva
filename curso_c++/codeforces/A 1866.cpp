3182c712f5fe801d4ea2b8cba372b4acd7f5ba30
Xiwei <huangg.xiwei@gmail.com>
2025-10-22 22:03:22 -0600

    hice un problema

---
+++ curso_c++/codeforces/A 1866.cpp
@@ -0,0 +1,25 @@
+// Autor: Mintwi
+// Fecha: 2025-10-22
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+        int n; cin >> n;
+
+        int ans = 1e5;
+
+        for(int i = 0; i < n; i++){
+            int A; cin >> A;
+
+            ans = min(ans,abs(A));
+        }
+
+        cout << ans << "\n";
+
+    return 0;
+}
