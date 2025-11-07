53dd297033bdcf8e8e224ca63b4ad9669195efa6
Xiweilol <huangg.xiwei@gmail.com>
2025-11-06 18:25:35 -0600

    ...

---
+++ curso_c++/codeforces/A 1627.cpp
@@ -0,0 +1,28 @@
+// Autor: Mintwi
+// Fecha: 2025-11-06
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while(t--){
+        int n,m,r,c;
+        cin >> n >> m >> r >> c;
+
+        vector <string> arr(n);
+
+        for(int i = 0; i < n; i++){
+            string s; cin >> s;
+            
+            for(int j = 0; j < (int)s.size(); j++){
+                
+            }
+        }
+    }
+
+    return 0;
+}
