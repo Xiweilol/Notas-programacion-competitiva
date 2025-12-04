d7f0ca19c5837d06452d5d25b1c1ff977009e7cb
Xiweilol <huangg.xiwei@gmail.com>
2025-12-04 17:47:48 -0600

    ...

--- curso_c++/Club algoritmia/Frog1.cpp
+++ curso_c++/Club algoritmia/Frog1.cpp
@@ -2,18 +2,24 @@
 // Fecha: 2025-12-04
 
 #include <bits/stdc++.h>
-#define ll long long
 using namespace std;
 
-int f(int i, vector <int>& h, vector <int>& dp){
-    if(dp[i] != -1) return dp[i];
-    if(i == 0) return 0;
-    if(i == 1) return abs(h[i] - h[i-1]);
+int step(int n,vector <int>& h, vector <int>& dp){
 
+    if(dp[n] != -1) return dp[n];
 
-    return dp[i] = min(f(i-1,h,dp) + abs(h[i] - h[i-1]), f(i-2,h,dp) + abs(h[i] - h[i-2]));
+    if(n == 0){
+        return 0;
+    }
 
+    if(n == 1){
+        return abs(h[n] - h[n-1]);
+    }
+
+    return dp[n] = min(step(n-1,h,dp) + abs(h[n] - h[n-1]), step(n-2,h,dp) + abs(h[n] - h[n-2]));
 }
+
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -21,10 +27,14 @@ int main(){
     int N; cin >> N;
 
     vector <int> h(N);
+
     vector <int> dp(N,-1);
+
     for(int i = 0; i < N; i++){
         cin >> h[i];
     }
-    cout << f(N-1,h,dp) <<  "\n";
+
+    cout << step(N-1,h,dp) << "\n";
+
     return 0;
 }
