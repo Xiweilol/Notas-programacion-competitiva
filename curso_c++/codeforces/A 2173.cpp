b8343f73be802afd6c5f78d33c8d6939c90fbe1e
Xiwei <huangg.xiwei@gmail.com>
2025-12-05 10:15:08 -0600

    ...

--- curso_c++/codeforces/A 2173.cpp
+++ curso_c++/codeforces/A 2173.cpp
@@ -16,7 +16,7 @@ int main(){
         string s; cin >> s;
 
         int cnt = 0;
-        int idx = -100;
+        int idx = -1000;
         for(int i = 0 ; i < n; i++){
             if(s[i] == '1'){
                 idx = i;
