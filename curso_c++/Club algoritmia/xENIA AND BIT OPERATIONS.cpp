e288cc68778dfaa18497040d16f78bce017a52c2
Xiweilol <huangg.xiwei@gmail.com>
2025-12-19 15:30:06 -0600

    ...

---
+++ curso_c++/Club algoritmia/xENIA AND BIT OPERATIONS.cpp
@@ -0,0 +1,17 @@
+// Autor: Mintwi
+// Fecha: 2025-12-09
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while(t--){
+        // tu código aquí
+    }
+
+    return 0;
+}
