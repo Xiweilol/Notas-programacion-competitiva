e8d400973b24c99c65b3945d1ec8c8e957ab1e7d
Xiwei <huangg.xiwei@gmail.com>
2026-01-01 16:16:59 -0600

    ...

--- curso_c++/atcoder/D - Knapsack1.cpp
+++ curso_c++/atcoder/D - Knapsack1.cpp
@@ -8,17 +8,19 @@ using namespace std;
 
 // i ir contando las rondas
 // j = el peso acumulando hasta ahorita,
-ll getMaximun(int N,ll W, int i, int j, vector<ll> &w, vector <ll> &v){
+ll getMaximun(int N,ll W, int i, int j, vector<ll> &w, vector <ll> &v, vector <vector<ll>> &memo){
+    if(j > W){
+        return -1e18;
+    }
     if(i > N){
         return 0;
     }
 
-    if(j > W){
-        return -1e18;
+    if(memo[i][j] != -1){
+        return memo[i][j];
     }
-
     //hacemos recursivo apra ahcer decision de tomar y no tomar
-    return max(v[i] + getMaximun(N,W,i+1,j + w[i],w,v), getMaximun(N,W,i+1,j,w,v));
+    return memo[i][j] = max(v[i] + getMaximun(N,W,i+1,j + w[i],w,v,memo), getMaximun(N,W,i+1,j,w,v,memo));
 
 }
 
@@ -32,11 +34,12 @@ int main(){
 
     vector <ll> w(N+1);
     vector <ll> v(N+1);
+    vector <vector<ll>> memo(N+1, vector<ll>(W+1,-1));
     for(int i = 1; i <= N; i++){
         cin >> w[i] >> v[i];
     }
 
-    cout << getMaximun(N,W,1,0,w,v) << "\n";
+    cout << getMaximun(N,W,1,0,w,v,memo) << "\n";
 
     return 0;
 }
