9b42e6d0c60000e9fcb783dcf1aeca7fcac3d65a
Xiwei <huangg.xiwei@gmail.com>
2026-02-07 22:31:15 -0600

    ...

---
+++ curso_c++/codeforces/GYM/106350.cpp
@@ -0,0 +1,17 @@
+// Autor: Mintwi
+// Fecha: 2026-02-07
+
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s; cin >> s;
+
+    cout << "FCDS\n";
+
+    return 0;
+}
