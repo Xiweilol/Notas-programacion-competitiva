324ab8bef53e66e61b5957545ad5f47a8047db3f
Xiwei <huangg.xiwei@gmail.com>
2026-02-20 22:25:16 -0600

    ...

--- curso_c++/CSES/1633.cpp
+++ curso_c++/CSES/1633.cpp
@@ -6,15 +6,29 @@ typedef long long ll;
 using namespace std;
 
 
+const int maxn = 1000007;
+ll a[maxn];
+int n; 
+ll dp(ll i){
+
+    if(i == n) return 1;
+
+    for(int j = 1; j <= n; j++){
+        if(i + j < n){
+            return dp(i+j);
+        }
+    }
+
+}
 
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int t; cin >> t;
-    while(t--){
-    }
+    cin >> n;
+
+    cout << dp(0) << "\n";
 
     return 0;
 }
--- curso_c++/codeforces/C 2112.cpp
+++ curso_c++/codeforces/C 2112.cpp
@@ -7,7 +7,7 @@ int main(){
     int t; cin >> t ;
 
     while(t--){
-        int n; cin >> n;
+        ll n; cin >> n;
 
         vector <ll> a(n);
 
@@ -15,30 +15,51 @@ int main(){
             cin >> a[i];
         }
 
-        int ans = 0;
+        ll ans = 0;
         
-        ll mxGlobal = a[n-1];
-        for(int i = n-1; i >= 0; i--){
-            ll firstOne = a[i];
-            for(int j = 0; j < i; j++){
-                ll secondOne = a[j];
-
-                int l = 0, r = n - 1;
-
+        for(int i = 0; i < n;i++){
+            ll x1 = -1, x2 = -1;
+            for(int j = i+1; j < n-1;j++){
+                int l = j+1, r = n - 1;
+                //para la condicion de a[i] + a[j] > X
+                //si cumple para x entonces cumple para x-1,x-2,x-3....
                 while(l <= r){
-                    int mid = l + r / 2;
-                    ll mx = LLONG_MIN;
-                    mx = max({firstOne,secondOne,a[mid]});
-                    ll sum = firstOne + secondOne + a[mid];
+                    int mid = (l + r) / 2;
 
-                    //considerar diferentes para el caso de sum de los3  > mxglobal y el suma de los 2 repasa al mas grande 
-                    if((mid != i && mid != j) && sum > mxGlobal && sum - mx > mx){
-
+                    if(a[i] + a[j] > a[mid]){
+                        x1 = mid;
+                        l = mid + 1;
+                    } else {
+                        r = mid - 1;
                     }
                 }
+
+                l = j+1, r = n - 1;
+                //segunda condicion
+                while(l <= r){
+                    
+                    int mid = (l + r ) / 2;
+                    //si cumple para x entonces se cumple para x+1,x+2,x+3....
+                    if(a[i] + a[j] + a[mid] > a[n-1]){
+                        x2 = mid;
+                        r = mid - 1;
+                    } else{
+                        l = mid + 1;
+                    }
+                }
+                //cout << "Indice i: " << i << "\n";
+                //cout << "Indice j: " << j << "\n";
+                //cout << "Valor de x1: " << x1 << "\n";
+                //cout << "Valor de x2: " << x2 << "\n";
+
+                if(x1 != -1 && x2 != -1){
+                    ans += (min(x1,n-1) - max(x2,0ll) >= 0 ? min(x1,n-1) - max(x2,0ll) + 1 : 0);
+                }
+
             }
-
-
         }
+
+        cout << ans << "\n";
     }
+
 }
