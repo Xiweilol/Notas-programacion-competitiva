d85661d23782e9fe9a7072cdcefc9bc2e71a126e
Xiwei <huangg.xiwei@gmail.com>
2026-02-22 19:53:05 -0600

    ...

--- curso_c++/CSES/1633.cpp
+++ curso_c++/CSES/1633.cpp
@@ -1,34 +1,34 @@
-// Autor: Mintwi
-// Fecha: 2026-02-19
-// No puedo con rate 1000 :( 
 #include <bits/stdc++.h>
-typedef long long ll;
 using namespace std;
 
+const int maxn = 1000002;
+const int mod = 1e9 + 7;
 
-const int maxn = 1000007;
-ll a[maxn];
-int n; 
-ll dp(ll i){
+bool vis[maxn];
+int memo[maxn];
+int n;
 
+///i es la suma que llevamos
+int dp(int i){
+    ///caso base
     if(i == n) return 1;
-
-    for(int j = 1; j <= n; j++){
-        if(i + j < n){
-            return dp(i+j);
+    ///checar si esta visitado
+    if(vis[i]) return memo[i];
+    vis[i] = true;
+    ///las tranciones
+    for(int j = 1; j <= 6; ++j){
+        if(i + j <= n){
+            memo[i]+= dp(i + j);
+            //if(memo[i] >= mod) memo[i]-= mod;
+            memo[i]%= mod;
         }
     }
-
+    return memo[i];///retornan la respuesta
 }
 
-
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
+    ios_base::sync_with_stdio(0); cin.tie(0);
     cin >> n;
-
     cout << dp(0) << "\n";
-
     return 0;
 }
