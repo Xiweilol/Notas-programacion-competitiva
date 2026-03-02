31d4b201ceee73f694e8535af39f9fc713625d35
Xiwei <huangg.xiwei@gmail.com>
2026-03-02 10:16:11 -0600

    ...

--- curso_c++/CSES/1193.cpp
+++ curso_c++/CSES/1193.cpp
@@ -1,51 +1,57 @@
 #include <bits/stdc++.h>
-
 typedef long long ll;
-
 using namespace std;
 
-const int maxn = 1000+5;
-char maps[maxn][maxn];
-int dist[maxn][maxn];
+const int maxn = 1000 + 5;
+
 int n,m;
-int Ax,Ay;
-int Bx,By;
+
 queue <pair<int,int>> cola;
+
+char mapa[maxn][maxn];
+
+int Ax,Ay,Bx,By;
+
 bool visi[maxn][maxn];
+char camino[maxn][maxn];
+
+//registrar los niveles
+int dist[maxn][maxn];
+
+//movimiento
 int dx[4] = {1,-1,0,0};
 int dy[4] = {0,0,1,-1};
-char pasos[maxn][maxn];
-char p[4] = {'D','U','R','L'};
+
+char pasos[4] = {'D','U','R','L'};
 
 bool valid(int x,int y){
     return x >= 0 && x < n && y >= 0 && y < m;
 }
-
-void bfs(int xs, int ys){
-    cola.push({xs,ys});
-    dist[xs][ys] = 0;
-    visi[xs][ys] = true;
+void bfs(){
+    visi[Ax][Ay] = true;
+    //empezamos en el nivel cero
+    dist[Ax][Ay] = 0;
+    cola.push({Ax,Ay});
 
     while(!cola.empty()){
         auto temp = cola.front();
         cola.pop();
         int x = temp.first;
         int y = temp.second;
+
         for(int i = 0; i < 4; i++){
+            //por cada nodo vamos a ir buscando sus vecinos
             int tempx = x + dx[i], tempy = y + dy[i];
 
-            if(valid(tempx,tempy) && !visi[tempx][tempy] && (maps[tempx][tempy] == '.' || maps[tempx][tempy] == 'B')){
-                //registrar passo que como fuimos llegando
-                pasos[tempx][tempy] = p[i];
-                //aumentar el nivel
-                dist[tempx][tempy] = dist[x][y] + 1;
+            if(valid(tempx,tempy) && !visi[tempx][tempy] && mapa[tempx][tempy] != '#'){
                 visi[tempx][tempy] = true;
-
                 cola.push({tempx,tempy});
+                dist[tempx][tempy] = dist[x][y] + 1;
+                camino[tempx][tempy] = pasos[i];
             }
         }
-
     }
+
 
 }
 int main(){
@@ -53,41 +59,41 @@ int main(){
     
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
-            cin >> maps[i][j];
+            cin >> mapa[i][j];
 
-            if(maps[i][j] == 'A'){
+            if(mapa[i][j] == 'A'){
                 Ax = i; Ay = j;
-            } else if(maps[i][j] == 'B'){
+            } else if(mapa[i][j] == 'B'){
                 Bx = i; By = j;
             }
         }
     }
 
-    bfs(Ax,Ay);
+    bfs();
 
     if(!visi[Bx][By]){
-        cout << "NO\n";
+        cout << "NO" << "\n";
     } else {
         cout << "YES\n";
 
-        string rr = "";
+        string chido = "";
 
-        int x = Bx, y =By;
+        int x = Bx, y = By;
 
         while(x != Ax || y != Ay){
-            rr.push_back(pasos[x][y]);
-
-            //si es U entonces cuando regresa lo mete hacia abajo
-            if(pasos[x][y] == 'U') x++;
-            else if(pasos[x][y] == 'D') x--;
-            else if(pasos[x][y] == 'L') y++;
-            else if(pasos[x][y] == 'R') y--;
+            chido.push_back(camino[x][y]);
+            if(camino[x][y] == 'U') x++;
+            else if(camino[x][y] == 'D') x--;
+            else if(camino[x][y] == 'L') y++;
+            else if(camino[x][y] == 'R') y--;
         }
 
-        cout << rr.size() << "\n";
-        reverse(rr.begin(),rr.end());
-        cout << rr << "\n";
+        cout << chido.size() << "\n";
 
+        reverse(chido.begin(),chido.end());
+
+        cout << chido << "\n";
     }
 
+    return 0;
 }
