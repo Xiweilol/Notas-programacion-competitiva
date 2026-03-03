2c713c0cd2d463cf17dfeb55564ce494ee6d542b
Xiwei <huangg.xiwei@gmail.com>
2026-03-03 16:10:15 -0600

    ...

--- curso_c++/CSES/1746.cpp
+++ curso_c++/CSES/1746.cpp
@@ -11,44 +11,47 @@ const ll mod = 1e9 + 7;
 int n,m;
 
 int a[maxn];
-int memo[maxn][maxn];
+int memo[maxn][102];
 
-bool visito[maxn][maxn];
+vector<vector<bool>> visito(maxn,vector <bool>(102,false));
 
-int dp(int i, int k){
-    //cuando llega final significa que 
+ll dp(int i, int k){
+    //por si es una k invalida
+    if(k < 1 || k > m) return 0; 
+    //cuando llega final significa que es una respuesta
     if(i == n) return 1;
-
-    //si el numero que paso ya no se cumple retorna 0
-    if(abs(a[i] - k) > 1) return 0;
-
-    //posicion actual menos el lemento que recibo
     if(visito[i][k]) return memo[i][k];
-
     visito[i][k] = true;
+    //si el numero que paso ya no se cumple retorna 0
+    
     //verificar si es 0
     if(a[i] != 0){
+        if(abs(a[i] - k) <= 1){
+            memo[i][k] += dp(i+1,a[i]);
+        }
         //simplemente lo avanzamos y no sumamos nada
-        memo[i][k] = dp(i+1,k);
     } else{
         memo[i][k] += (dp(i+1,k) + dp(i+1,k+1) + dp(i+1,k-1)) % mod;
     }
 
-    return (memo[i][k]) % mod; 
+    return memo[i][k]; 
 }
 int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
     cin >> n;
     cin >> m;
 
     for(int i = 0; i < n; i++){
         cin >> a[i];
     }
-    int ans = 0;
+    ll ans = 0;
+    // si el primer oes diferente de 0, solo pasamos 
     if(a[0] != 0){
         ans += dp(1,a[0]);
     } else {
+        //inicia en 0
         for(int i = 1; i <= m; i++){
-            ans += dp(1,a[i]);
+            ans += (dp(1,i)) % mod;
         }
     }
 
