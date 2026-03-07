3238aa7bc47361f4688bd678c9e810871456407f
Xiwei <huangg.xiwei@gmail.com>
2026-03-07 10:15:37 -0600

    ...

--- curso_c++/CSES/1639.cpp
+++ curso_c++/CSES/1639.cpp
@@ -6,8 +6,14 @@ typedef long long ll;
 using namespace std;
 
 const ll mod = 1000000000 + 7;
-string n, m;
 
+const int maxn = 5000 + 5;
+string n, m;
+//el estado son las indices que llevamos carreando
+int dp(int i, int j){
+    //caso base
+    if()
+}
 
 int main(){
     ios_base::sync_with_stdio(0);
