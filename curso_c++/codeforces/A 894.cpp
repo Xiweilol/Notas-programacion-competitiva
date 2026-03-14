065d20fe520ac7eafcb01da59db6d9293001feea
Xiwei <huangg.xiwei@gmail.com>
2026-03-14 17:02:26 -0600

    ...

--- curso_c++/codeforces/A 894.cpp
+++ curso_c++/codeforces/A 894.cpp
@@ -6,11 +6,20 @@ int main(){
     string n; cin >> n;
 
     int len = n.size();
+    int cnt = 0;
     for(int i = 0; i < len; ++i){
         if(n[i] == 'Q'){
             for(int j = i + 1; j < len; j++){
-                if()
+                if(n[j] == 'A'){
+                    for(int k = j + 1; k < len; k++){
+                        if(n[k] == 'Q'){
+                            cnt++;
+                        }
+                    }
+                }
             }
         }
     }
+
+    cout << cnt << "\n";
 }
