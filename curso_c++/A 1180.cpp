745504f495af3b73c1880d962051b4676c26be58
Xiwei <huangg.xiwei@gmail.com>
2026-03-16 15:08:59 -0600

    ...

---
+++ curso_c++/A 1180.cpp
@@ -0,0 +1,18 @@
+// Autor: Mintwi
+// Fecha: 2026-03-16
+// No puedo con rate 1000 :( 
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+const ll mod = 1000000000 + 7;
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n; cin >> n;
+
+    cout << (n*n)  + (n-1)*(n-1) << "\n";
+
+    return 0;
+}
