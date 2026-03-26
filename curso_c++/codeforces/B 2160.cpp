3b1a8d10d2674a1788dbea6d8b4c091cbc83f7b6
Xiwei <huangg.xiwei@gmail.com>
2026-03-26 07:21:54 -0600

    ...

---
+++ curso_c++/codeforces/B 2160.cpp
@@ -0,0 +1,37 @@
+// mintwi
+
+#include <bits/stdc++.h>
+
+typedef long long ll;
+using namespace std;
+
+int main(){
+    int t; cin >> t;
+
+    while(t--){
+        int n; cin >> n;
+
+        vector <ll> b(n+1);
+
+        for(int i = 1; i <= n; i++) cin >> b[i];
+
+        vector <int> ans(n+1,0);
+        
+        ans[1] = 1;
+
+        for(int i = 2; i <= n; i++){
+            ll diff = b[i] - b[i-1];
+            if(diff == i){
+                ans[i] = ans[i-1] + 1;
+            } else {
+                ans[i] = ans[i-diff];
+            }
+        }
+
+        for(int i = 1; i <= n; i++){
+            cout << ans[i] << " ";
+        }
+
+        cout << "\n";
+    }
+}
