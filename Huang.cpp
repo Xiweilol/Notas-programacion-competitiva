ca20a9d155760956506007bf3c0ee4339f16e744
Xiwei <huangg.xiwei@gmail.com>
2026-04-21 15:09:20 -0600

    ...

--- Huang.cpp
+++ Huang.cpp
@@ -1,10 +1,42 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
 using namespace std;
+
+
+int esta(int x,int*a,int i){
+    for(int j = 0; j < i; j++){
+        if(a[j] == x){
+            return 1;
+        }
+    }
+
+    return 0;
+}
 
 int main() {
     
-    long long a = LONG_LONG_MAX;
+    //arreglo de numeros generados random
+    int arr[10]; 
+    //generar la semila de eleccion de numero
+    srand(time(NULL));
+    int i = 0;
+    while(i < 10){
 
+        //elige un numero randon de 0 a 49
+        int x = rand() % 50;
+        //checa si esta en el arreglo
+        if(!esta(x,arr,i)){
+            // si no esta, lo asignamos y avanzamos i
+            arr[i] = x;
+            i++;
+        } 
+    }
 
-    cout << a << "\n";
-}
+    for(int i = 0; i < 10; i++){
+        printf("%d ",arr[i]);
+    }
+
+    return 0;
+}   
