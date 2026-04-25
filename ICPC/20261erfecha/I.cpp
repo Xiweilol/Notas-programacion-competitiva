5ec7117b2d542a84bd4f2a3d82726579b40556d3
Xiwei <huangg.xiwei@gmail.com>
2026-04-24 23:06:22 -0600

    ...

--- ICPC/20261erfecha/I.cpp
+++ ICPC/20261erfecha/I.cpp
@@ -4,31 +4,76 @@ typedef long long ll;
 using namespace std;
 
 int main(){
-    int n; cin >> n;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 
-    vector <ll> a(n);
+    int n;
+    cin >> n;
 
+    vector<ll> a(n);
     for(int i = 0; i < n; i++) cin >> a[i];
 
-    string s; cin >> s;
+    string s;
+    cin >> s;
 
-    string new_one = "";
+    vector<ll> pesos;
+    string signos = "";
 
-    for(char c : s){
-        if(c == '=') continue;
+    ll actual = a[0];
 
-        new_one += c;
-    
-    }
-
-    vector <int> ans(new_one.size());
-
-    char temp;
-
-    for(int i = 0; i < new_one.size(); i++){
-        if((i == 0 && new_one[i] = '<')){
-            
+    for(int i = 0; i < n - 1; i++){
+        if(s[i] == '='){
+            actual += a[i + 1];
+        } else {
+            pesos.push_back(actual);
+            signos += s[i];
+            actual = a[i + 1];
         }
     }
-}
 
+    pesos.push_back(actual);
+
+    int m = pesos.size();
+
+    vector<ll> ans(m, 1);
+
+    // Pasada izquierda a derecha para los <
+    for(int i = 0; i < m - 1; i++){
+        if(signos[i] == '<'){
+            ans[i + 1] = max(ans[i + 1], ans[i] + 1);
+        }
+    }
+
+    // Pasada derecha a izquierda para los >
+    for(int i = m - 2; i >= 0; i--){
+        if(signos[i] == '>'){
+            ans[i] = max(ans[i], ans[i + 1] + 1);
+        }
+    }
+
+    vector<ll> ansFinal;
+
+    int pos = 0;
+    ansFinal.push_back(ans[pos]);
+
+    for(int i = 0; i < n - 1; i++){
+        if(s[i] != '='){
+            pos++;
+        }
+        ansFinal.push_back(ans[pos]);
+    }
+
+    ll sum = 0;
+
+    for(int i = 0; i < n; i++){
+        sum += a[i] * ansFinal[i];
+    }
+
+    cout << sum << "\n";
+
+    for(int i = 0; i < n; i++){
+        cout << ansFinal[i] << " ";
+    }
+
+    return 0;
+}
