a5dcab760138bcca534c35e79dea34b54c713c34
Xiwei <huangg.xiwei@gmail.com>
2026-05-18 22:59:06 -0600

    ...

--- curso_c++/codeforces/C1 2228.cpp
+++ curso_c++/codeforces/C1 2228.cpp
@@ -6,26 +6,42 @@ typedef long long ll;
 using namespace std;
 
 const ll mod = 1000000000 + 7;
+char d1;
+char d2;
+ll ans;
+ll a;
+void generar(int pos,int len,string s){
+    //ya generamos un numero con digito que queremos
+    if(pos == len){
+        ll b = stoll(s);
+        ans = min(ans,abs(a-b));
+        return;
+    }
+
+    generar(pos+1,len,s+d1);
+    generar(pos+1,len,s+d2);
+
+}
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
     int t; cin >> t;
     while(t--){
-        ll a; cin >> a;
+        cin >> a;
         int n; cin >> n;
 
-        ll d1,d2; cin >> d1 >> d2;
+        cin >> d1 >> d2;
 
-        int tempd1 = d1;
-        int tempd2 = d2;
-        ll ans = LLONG_MAX;
-        for(int i = 0; i <= 17; i++){
-            ans = min({ans,abs(a-d1),abs(a-d2)});
-            d1 = (d1*10) + tempd1;
-            d2 = (d2*10) + tempd2; 
+        ans = LLONG_MAX;
 
+        int largo = to_string(a).size();
+        
+        for(int i = max(1,largo-1); i <= largo+1; i++){
+            generar(0,i,"");
         }
+
+        cout << ans << "\n";
     }
 
     return 0;
--- curso_c++/prueba.cpp
+++ curso_c++/prueba.cpp
@@ -1,25 +1,23 @@
-#include <iostream>
-#include <cstdint>
-#include <vector>
-#include <map>
-#include <algorithm>
-#include <set>
-#include <string>
+#include<bits/stdc++.h>
 using namespace std;
 
-typedef struct NODO *nodito;
-//parametro por valor
-int lol(int x, int y){
-   return x + y;
+typedef long long ll;
+
+const int maxn = 100005;
+
+int longitud = 5;
+
+void f(int i, int num){
+    if(i == longitud){
+        cout << "num: " << num << "\n";
+        return;
+    }
+    f(i + 1, num + (1 << i));
+    f(i + 1, num);
 }
 
-struct NODO{
-   string miembro;
-   int edad;
-   float altura;
-};
-
-
 int main(){
-   cout << 108 % 16 << "\n";
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    f(0, 0);
+    return 0;
 }
