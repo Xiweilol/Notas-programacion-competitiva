61b539fc1e4fbb513d40845cda76b13329427904
Xiwei <huangg.xiwei@gmail.com>
2026-05-28 18:50:48 -0600

    ...

---
+++ curso_c++/codeforces/A 1550.cpp
@@ -0,0 +1,27 @@
+// Autor: Mintwi
+// Fecha: 2026-05-28
+// No puedo con rate 1000 :( 
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+const ll mod = 1000000000 + 7;
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t; cin >> t;
+    while(t--){
+        int s; cin >> s;
+
+        int ans = 1;
+
+        while(ans * ans < s){
+            ans++;
+        }
+
+        cout << ans << "\n";
+    }
+
+    return 0;
+}
